bdd15dea2e40b080c74d19782819cfbbc6657a93
Aiday Marlen Kyzy <aidaymarlenkyzy@gmail.com>
2021-10-19 16:23:27 +0200

    Update user.js

--- api/final/resolvers/user.js
+++ api/final/resolvers/user.js
@@ -1,6 +1,7 @@
 module.exports = {
   // Resolve the list of notes for a user when requested
   notes: async (user, args, { models }) => {
+    // finding the notes, where the author is equal to the current user id
     return await models.Note.find({ author: user._id }).sort({ _id: -1 });
   },
   // Resolve the list of favorites for a user when requested
